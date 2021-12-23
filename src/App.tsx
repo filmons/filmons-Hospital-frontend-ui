6135f46ba420180bc353986484150a9bb027be08
vishnugopy <vishnugoby0010@gmail.com>
2021-12-23 15:12:02 +0100

    LOG IN SIGNUP

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.scss';
+import "./App.scss";
+import Login from "../src/Pages/Login";
+import Signup from "../src/Pages/Signup";
 import Header from './Components/MenuAppBar';
-// import Home from './Pages/Home';
+
 
 function App() {
   return (
     <Router>
-    <Header/>
-    <Routes>
-      {/* <Route path="/" element={<Home/>} /> */}
-    </Routes>
-  </Router>
+       <Header/>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
