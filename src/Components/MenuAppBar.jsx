61541d7994be26c98ca81ab70e7ff087923f3803
vishnugopy <vishnugoby0010@gmail.com>
2022-04-11 14:06:09 +0200

    fix

--- src/Components/MenuAppBar.jsx
+++ src/Components/MenuAppBar.jsx
@@ -19,7 +19,13 @@ import { Box } from "@material-ui/core";
 const useStyles = makeStyles({
   bar: {
     background: "linear-gradient(45deg, #22577E 30%, #2ca7ff 90%)",
-    top: 0,
+  },
+  buttonburger: {
+    color: "white",
+    padding: "10px",
+    "&:hover": {
+      cursor: "pointer",
+    },
   },
 });
 
@@ -48,22 +54,13 @@ export default function MenuAppBar() {
             </Link>
           </Typography>
 
-          {/* {auth && } */}
-          <AccountCircle />
           <div>
-            <AppBar position="static">
-              <Toolbar
-                style={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <IconButton color="inherit" onClick={toggleDrawer}>
-                  <MenuIcon />
-                </IconButton>
-              </Toolbar>
-            </AppBar>
+            <MenuIcon className={classes.buttonburger} onClick={toggleDrawer} />
+
             <Drawer
               open={drawer}
               onClose={toggleDrawer}
-              style={{ width: 600 }}
+              style={{ width: "250px" }}
               anchor={"top"}
             >
               <List>
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,16 @@
 import { user, usersignup } from "../types/user.types";
 import http from "./config";
 
-export const signIn = async (user: Omit<user, "access_token">): Promise<any> => {
-    return await http.post('patient/login', user);
-}
+export const signIn = async (
+  user: Omit<user, "access_token">
+): Promise<any> => {
+  return await http.post("patient/login", user);
+};
 
-export const signUp = async (user: usersignup ): Promise<any> => {
-    return await http.post('patient/signup', user);
-}
+export const signUp = async (user: usersignup ,) : Promise<any> => {
+  return await http.post("patient/signup", user);
+};
 
-export const getByUser = async () : Promise<any> => {
-    return await http.get('/patient');
-}
+export const getByUser = async (): Promise<any> => {
+  return await http.get("/patient");
+};
