04218c5e41fd2f576f072a44c0b28f44e61b3d69
vishnugopy <vishnugoby0010@gmail.com>
2022-04-11 17:07:12 +0200

    home page with search and add functions

--- src/services/praticien.service.ts
+++ src/services/praticien.service.ts
@@ -1,5 +1,5 @@
 import http from "./config";
 
-export const Search = async (input: any): Promise<any> => {
-  return await http.get("/praticioner/search?query=" + input + "&");
+export const Search = async (input: string): Promise<any> => {
+  return await http.get("/practitioner/search?query=" + input);
 };
