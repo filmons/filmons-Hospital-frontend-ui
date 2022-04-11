f0ee08768c655695301ed67dea4c10d84e987103
vishnugopy <vishnugoby0010@gmail.com>
2022-04-11 16:11:54 +0200

    home page with search and add functions

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 function Card(data: any) {
   const hospital = data.hospital;
   return (
-    <section className="card">
+    <Link to={"/" + hospital.id} className="card">
       <h2>{hospital.name}</h2>
       <p>{hospital.telephone}</p>
       <p>{hospital.address}</p>
@@ -18,8 +19,8 @@ function Card(data: any) {
         <p className="tag">tag</p>
         <p className="tag">tag</p>
         <p className="tag">tag</p>
-        </div>
-     </section>
+      </div>
+    </Link>
   );
 }
 
