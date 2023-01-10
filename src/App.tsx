bdc48f022176b5dcdba1a0256e46a8afd1139763
filmons <filmonseare@gmail.com>
2023-01-10 16:56:47 +0100

    m

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Signup from "../src/Pages/Signup";
 import Header from "./Components/MenuAppBar";
 import DetailsPage from "../src/Pages/DetailsPage";
 import DashBoard from "./Pages/DashBoard";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/details" element={<DetailsPage />} />
         <Route path="/dashboard" element={<DashBoard />} />
+        <Route  path="*"  element={<NotFound/>} />
       </Routes>
     </Router>
   );
---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>404 - Not Found!</h1>
+    <div className="tags">
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        <p className="tag">tag</p>
+        </div>
+  </div>
+);
+
+export default NotFound;
